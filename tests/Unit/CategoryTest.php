053066b3635f75a9e3b117ea7f9bf7efc3fe1484
daniel munn <dannymunn1995@gmail.com>
2019-04-05 13:06:18 +0100

    Removed unused tests and added category test

--- tests/Unit/CategoryTest.php
+++ tests/Unit/CategoryTest.php
@@ -5,14 +5,14 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class CategoryTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCategoryTest()
     {
         $this->assertTrue(true);
     }
